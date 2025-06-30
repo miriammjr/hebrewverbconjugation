7963a325392dfc4b3b9ffb35216adae06a07042b
miriammjr <mirrundell@gmail.com>
2025-06-30 01:55:13 -0700

    some text buttons + input box working

--- consoletest.js
+++ consoletest.js
@@ -1,4 +1,26 @@
 // import "/paal/paal-shlemim-efol.js";
+const input = document.getElementById("input");
+document.onload = function () {
+  input.innerHTML = "";
+};
+
+const clear = document.getElementById("clear");
+const alef = document.getElementById("alef");
+const samekh = document.getElementById("samekh");
+const vav = document.getElementById("vav");
+const gimel = document.getElementById("gimel");
+const resh = document.getElementById("resh");
+const yod = document.getElementById("yod");
+const memsofit = document.getElementById("memsofit");
+const tav = document.getElementById("tav");
+
+const vavholam = document.getElementById("vav-holam");
+const tzere = document.getElementById("tzere");
+const segol = document.getElementById("segol");
+const shva = document.getElementById("shva");
+const khirik = document.getElementById("khirik");
+let letters = [alef, samekh, vav, gimel, resh, yod, memsofit, tav];
+let vowels = [vavholam, tzere, segol, shva, khirik];
 
 const test1 = new PaalShlemimEfol("ר", "ג", "ס");
 const curr = document.getElementById("all");
@@ -9,3 +31,24 @@ console.log(test1.presetFemSing());
 console.log(test1.presentMascPlural());
 
 curr.innerHTML = `<p>${test1.presentMascSing()}</p>`;
+
+function addText(x) {
+  input.innerHTML = input.innerHTML + x;
+  console.log("PRESSED");
+}
+
+for (let i = 0; i < letters.length; i++) {
+  letters[i].addEventListener("click", (event) => {
+    addText(letters[i].value);
+  });
+}
+
+for (let i = 0; i < vowels.length; i++) {
+  vowels[i].addEventListener("click", (event) => {
+    addText(vowels[i].value);
+  });
+}
+
+clear.addEventListener("click", (event) => {
+  input.innerHTML = "";
+});
