e726a292eb9a3c40e54dde8f4f0614cfefb2dcbe
miriammjr <mirrundell@gmail.com>
2025-06-30 13:09:05 -0700

    added all vowels and letters

--- consoletest.js
+++ consoletest.js
@@ -10,22 +10,90 @@ const start = document.getElementById("start");
 const checkButton = document.getElementById("check");
 const backspace = document.getElementById("backspace");
 const clear = document.getElementById("clear");
-const alef = document.getElementById("alef");
-const samekh = document.getElementById("samekh");
-const vav = document.getElementById("vav");
-const gimel = document.getElementById("gimel");
-const resh = document.getElementById("resh");
-const yod = document.getElementById("yod");
-const memsofit = document.getElementById("memsofit");
-const tav = document.getElementById("tav");
 
-const vavholam = document.getElementById("vav-holam");
+// LETTERS
+const alef = document.getElementById("alef");
+const bet = document.getElementById("bet");
+const gimel = document.getElementById("gimel");
+const dalet = document.getElementById("dalet");
+const he = document.getElementById("he");
+const vav = document.getElementById("vav");
+const zayin = document.getElementById("zayin");
+const khet = document.getElementById("khet");
+const tet = document.getElementById("tet");
+const yod = document.getElementById("yod");
+const kaf = document.getElementById("kaf");
+const kafsofit = document.getElementById("kafsofit");
+const lamed = document.getElementById("lamed");
+const mem = document.getElementById("mem");
+const memsofit = document.getElementById("memsofit");
+const nun = document.getElementById("nun");
+const nunsofit = document.getElementById("nunsofit");
+const samekh = document.getElementById("samekh");
+const ayin = document.getElementById("ayin");
+const pe = document.getElementById("pe");
+const pesofit = document.getElementById("pesofit");
+const tsadi = document.getElementById("tsadi");
+const tsadisofit = document.getElementById("tsadisofit");
+const kof = document.getElementById("kof");
+const resh = document.getElementById("resh");
+const shin = document.getElementById("shin");
+const sin = document.getElementById("sin");
+const tav = document.getElementById("tav");
+const letters = [
+  alef,
+  bet,
+  gimel,
+  dalet,
+  he,
+  vav,
+  zayin,
+  khet,
+  tet,
+  yod,
+  kaf,
+  kafsofit,
+  lamed,
+  mem,
+  memsofit,
+  nun,
+  nunsofit,
+  samekh,
+  ayin,
+  pe,
+  pesofit,
+  tsadi,
+  tsadisofit,
+  kof,
+  resh,
+  shin,
+  sin,
+  tav,
+];
+
+// VOWELS
+const holam = document.getElementById("holam");
 const tzere = document.getElementById("tzere");
 const segol = document.getElementById("segol");
 const shva = document.getElementById("shva");
 const khirik = document.getElementById("khirik");
-let letters = [alef, samekh, vav, gimel, resh, yod, memsofit, tav];
-let vowels = [vavholam, tzere, segol, shva, khirik];
+const patakh = document.getElementById("patakh");
+const dagesh = document.getElementById("dagesh");
+const kamatz = document.getElementById("kamatz");
+const shuruk = document.getElementById("shuruk");
+const kubutz = document.getElementById("kubutz");
+let vowels = [
+  holam,
+  tzere,
+  segol,
+  shva,
+  khirik,
+  patakh,
+  dagesh,
+  kamatz,
+  shuruk,
+  kubutz,
+];
 
 const test1 = new PaalShlemimEfol("ר", "ג", "ס");
 // const curr = document.getElementById("all");
